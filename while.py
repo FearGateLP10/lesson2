035211b720e1c58ec6629a34c1667a8ea38ad1f5
Firgat Musin <f.m.musin@gmail.com>
2018-09-16 13:00:00 +0500

    changed while.py added except/error

--- while.py
+++ while.py
@@ -9,25 +9,41 @@ import random
 # Пользователь: Что делаешь?
 # Программа: Программирую
 
+# Перепишите функцию ask_user() из задания про while, чтобы она перехватывала KeyboardInterrupt, 
+# писала пользователю "Пока!" и завершала работу при помощи оператора break
 
-my_dict = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Привет": "Ну, здравствуй", 
+
+answers = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Привет": "Ну, здравствуй", 
 			"Чем занимаешься?": "Играю", "Как ты?": "Норм", "Ясно": "Понятно (="
 }
 
-foo = ['Не понял ', "Пиши по-русски ", "Что? ", "Ты что-то хотел спросить у меня?", "Повтори-ка "]
+foo = ['Не понял ', "Пиши по-русски ", "Что? ", "Ты что-то хотел спросить у меня? ", "Повтори-ка "]
 
 
-def ask_user():
-	answer = 'Привет!'
-	print(answer)
-	while not answer in my_dict.keys():
-		answer = input()
-		print(random.choice(foo))
-	print(my_dict[answer])
+def get_answer(question, answers):
+	return answers.get(question)
+
+
+def ask_user(answers):
+	print('Привет!')
+	while True:
+		user_input = input()
+		answer = get_answer(user_input, answers)
+
+		if user_input == 'пока':
+			print('До скорого!')
+			break
+		elif answer:
+			print(answer)
+		else:
+			print(random.choice(foo))
 
 
 def main():
-	ask_user()
+	try:
+		ask_user(answers)
+	except KeyboardInterrupt:
+		print('лавно поговорили...')
 	return None
 
 
